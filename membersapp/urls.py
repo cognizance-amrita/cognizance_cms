e8fa31fb4e36f947560740ca5faa9743ed714daf
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-12-19 17:39:36 +0530

    Added Tasks display based on member domain in membersapp

--- membersapp/urls.py
+++ membersapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard,name='dashboard'),
-    path('dashboard/',views.dashboard,name='dashboard')
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('tasks/', views.member_tasks, name='tasks')
     
 ]
--- membersapp/views.py
+++ membersapp/views.py
@@ -10,5 +10,11 @@ def dashboard(request):
     return render(request, 'membersapp/member-dashboard.html')
 
 def member_tasks(request):
-
-    return render(request, 'membersapp/member-tasks.html')
+    user = User.objects.get(username=request.user.username)
+    groups =  user.groups.all()
+    tasks = Task.objects.all()
+    domain = ''
+    for t in tasks:
+        if user.groups.filter(name=t.group).exists():
+            domain = t.group
+    return render(request, 'membersapp/member-tasks.html', {'groups':groups, 'tasks':tasks, 'domain':domain})
