815b0d1203c44cd959f43e461c9bda4cf9f1681b
TechieNK <nk2indian@gmail.com>
2020-12-28 12:12:15 +0530

    Added changes

--- cognizance_cms/urls.py
+++ cognizance_cms/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,5 @@ urlpatterns = [
     path('adminapp/', include('adminapp.urls'), name='admin-dashboard'),
     path('membersapp/', include('membersapp.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
