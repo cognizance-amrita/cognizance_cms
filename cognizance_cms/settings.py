9ad2afc95df7f704886fcdca64b8e061771b2018
techwizzie <sanjayipscoc@gmail.com>
2021-03-01 19:59:35 +0530

    Updated db resolutions

--- cognizance_cms/settings.py
+++ cognizance_cms/settings.py
@@ -106,6 +106,9 @@ DATABASES = {
 }
 
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
