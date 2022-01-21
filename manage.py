e67dfe1c1d145cc8ede0baa41bb26023120c01e3
harsha <v.harshavardhan3@gmail.com>
2022-01-22 00:04:43 +0530

    Security updates environment variables added

--- manage.py
+++ manage.py
@@ -2,9 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotnev
 
 def main():
+    dotnev.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cognizance_cms.settings')
     try:
         from django.core.management import execute_from_command_line
